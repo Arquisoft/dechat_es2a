669a2496584922a93f7b573f09eec09ca04f91a8
DiegoTrapiello <diego_trapiello@hotmail.com>
2019-03-01 22:45:20 +0100

    Documentar codigo createNewFOlder()

--- src/ASW/src/app/card/card.component.ts
+++ src/ASW/src/app/card/card.component.ts
@@ -89,17 +89,23 @@ export class CardComponent implements OnInit  {
     this.auth.solidSignOut();
   }
 
-
+  //Usando la libreria de https://github.com/jeff-zucker/solid-file-client puedo crear y eliminar archivos, ficheros, etc. Es recomendable leer su README
   private createNewFolder(){
 
+    //Para crear la carpeta necesito una ruta que incluya el nombre de la misma. 
+    //Obtengo el ID del usuario y sustituyo  lo irrelevante por la ruta de public/NombreCarpeta
     let solidId = this.rdf.session.webId;
     let stringToChange = "/profile/card#me";
     solidId = solidId.replace(stringToChange,"/public/pruebaASW")
-    
+
+    //Necesito logearme en el cliente para que me de permiso, sino me dara un error al intentar
+    //crear la carpeta. Como ya estoy en sesion no abre nada pero si se abre la consola se ve 
+    // que se ejecuta correctamente.
     this.fileClient.popupLogin().then( webId => {
        console.log( `Logged in as ${webId}.`)
   }, err => console.log(err) );
-
+  
+    //Le paso la URL de la carpeta y se crea en el pod. SI ya esta creada no se si la sustituye o no hace nada
     this.fileClient.createFolder(solidId).then(success => {
       console.log(`Created folder ${solidId}.`);
     }, err => console.log(err) );
