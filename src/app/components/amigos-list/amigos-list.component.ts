fecaab33c8d694f5a4b4d3f3b917634aa3bb79b7
adanvetusta <adanvetusta@gmail.com>
2019-03-12 18:22:42 +0100

    Probando subida de src

---
+++ src/app/components/amigos-list/amigos-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { RdfService } from '../../services/rdf.service';
+
+import {of} from 'rxjs';
+import {variable} from '../../../assets/types/rdflib';
+
+
+@Component({
+  selector: 'app-amigos-list',
+  templateUrl: './amigos-list.component.html',
+  styleUrls: ['./amigos-list.component.css']
+})
+export class AmigosListComponent implements OnInit {
+
+  amigos = [];
+
+  constructor(private router: Router, private rdf: RdfService) {
+  }
+
+  ngOnInit() {
+
+    this.loadFriends();
+
+  }
+
+  loadFriends() {
+    const list_friends = this.rdf.getFriends();
+
+    if (list_friends) {
+      console.log(list_friends);
+      let i = 0;
+      this.amigos = list_friends;
+    }
+  }
+}
