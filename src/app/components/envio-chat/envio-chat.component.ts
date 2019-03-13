d8786353077e3bd4c52d44d27590ca7860fad8fc
adanvetusta <adanvetusta@gmail.com>
2019-03-14 00:35:32 +0100

    Leer del POD: documento específico (TXT)

--- src/app/components/envio-chat/envio-chat.component.ts
+++ src/app/components/envio-chat/envio-chat.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { RdfService } from '../../services/rdf.service';
 import { ActivatedRoute, Params } from '@angular/router';
-const Message = require('../../Modelo/Mensaje');
 
 @Component({
   selector: 'app-envio-chat',
@@ -12,6 +11,7 @@ export class EnvioChatComponent implements OnInit {
 
     fileClient: any;
     ruta_seleccionada: string;
+    contenido: any;
 
     constructor(private rdf: RdfService, private rutaActiva: ActivatedRoute) {
         this.rutaActiva.params.subscribe(data => {
@@ -24,76 +24,67 @@ export class EnvioChatComponent implements OnInit {
 
     ngOnInit() {
         this.fileClient = require('solid-file-client');
+        const name = this.getUserByUrl(this.ruta_seleccionada);
+        this.createNewFolder('dechat', '/public/');
+        this.createNewFolder(name, '/public/dechat/');
     }
 
     private getUserByUrl(ruta: string): string {
-        let sinhttp = ruta.replace('https://', '');
-        let user = sinhttp.split('.')[0];
+        let sinhttp;
+        sinhttp = ruta.replace('https://', '');
+        const user = sinhttp.split('.')[0];
         return user;
-
     }
 
-    async createNewFolder() {
-
+    private createNewFolder(name: string, ruta: string) {
         //Para crear la carpeta necesito una ruta que incluya el nombre de la misma.
         //Obtengo el ID del usuario y sustituyo  lo irrelevante por la ruta de public/NombreCarpeta
         let solidId = this.rdf.session.webId;
-        let stringToChange = "/profile/card#me";
-        let user = this.getUserByUrl(this.ruta_seleccionada);
-        let path = "/public/" + user;
+        let stringToChange = '/profile/card#me';
+        let path = ruta + name;
         solidId = solidId.replace(stringToChange, path);
 
         //Necesito logearme en el cliente para que me de permiso, sino me dara un error al intentar
         //crear la carpeta. Como ya estoy en sesion no abre nada pero si se abre la consola se ve
         // que se ejecuta correctamente.
-        this.fileClient.popupLogin().then(webId => {
-            console.log(`Logged in as ${webId}.`)
-        }, err => console.log(err));
 
-        //Le paso la URL de la carpeta y se crea en el pod. SI ya esta creada no se si la sustituye o no hace nada
-        this.fileClient.createFolder(solidId).then(success => {
-            console.log(`Created folder ${solidId}.`);
-        }, err => console.log(err));
-
-
-
-        console.log("SolidID: " + solidId);
-        console.log("Emisor: " + this.rdf.session.webId);
-        console.log("Receptor: " + this.ruta_seleccionada);
-
-        var mensaje = new Message(this.rdf.session.webId, this.ruta_seleccionada, 'probando');
-        const mensajes = [];
-        mensajes.push(mensaje);
-
-        const messagesJSON = this.buildJSONmessages(this.rdf.session.webId.replace('https://', '').replace('/profile/card#me', ''), this.ruta_seleccionada.replace('https://', '').replace('/profile/card#me', ''), mensajes);
-
-        // var messagesJSON = txtFileBuilder.buildJSONmessages(userID, partnerID, messages);
-
-        this.fileClient.createFile(solidId, messagesJSON).then(200);
-
-
+        console.log("SOLID ID: " + solidId);
+        this.buildFolder(solidId);
 
     }
 
-    buildJSONmessages(emisor, receptor, messages)
-    {
+    //method that creates the folder using the solid-file-client lib
+    private buildFolder(solidId) {
+        this.fileClient.readFolder(solidId).then(folder => {
+            console.log(`Read ${folder.name}, it has ${folder.files.length} files.`);
+        }, err => {
+            //Le paso la URL de la carpeta y se crea en el pod. SI ya esta creada no se si la sustituye o no hace nada
+            this.fileClient.createFolder(solidId).then(success => {
+                console.log(`Created folder ${solidId}.`);
+            }, err1 => console.log(err1));
 
-        var actualizacion = new Date().getTime();
+        });
+    }
 
-        var tostr = JSON.stringify({'webid_sender': emisor, 'webid_receiver': receptor, 'lastupdate': actualizacion, 'messages': '__'});
-        var sepsa = tostr.split('"__"');
-        var ret = '////' + sepsa[0] + '[';
-        var i;
-        for (i = 0; i < messages.length; i++) {
-            ret = ret + messages[i].serialize();
-            if (i != messages.length - 1) {
-                ret = ret + ',';
-            }
-            else {
-                ret = ret + ']' + sepsa[1] + '////';
-            }
-        }
-        console.log(ret);
-        return ret;
+    private async readMessage(url) {
+        var message = await this.searchMessage(url);
+        console.log(message);
+        return message;
+    }
+
+    //method that search for a message in a pod
+    private searchMessage(url) {
+        console.log("MENSAJE PARA BUSCAR: " + url);
+        return this.fileClient.readFile(url).then(body => {
+            console.log(`File	content is : ${body}.`);
+            document.write(body);
+            return body;
+        }, err => console.log(err));
+
+    }
+
+    private ver_mensajes() {
+        let url = 'https://adanvetusta.solid.community/public/dechat/adanfernandezsanchez/Conversation2.txt';
+        let messageContent = this.searchMessage(url);
     }
 }
