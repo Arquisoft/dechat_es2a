b365e4eaa519bb65ed03bb212ae94271b7d122b9
Raúl García Arenas <UO257275@uniovi.es>
2019-03-29 09:55:51 +0100

    Creacion de carpeta

---
+++ src/test/asw/aswTest.ts
@@ -0,0 +1,25 @@
+import {file} from '@babel/types';
+
+const assert = import('assert');
+const fileClient = import('solid-file-client');
+// @ts-ignore
+import {EnvioChatComponent} from '../src/app/component/envio-chat';
+
+
+const usuario = {
+        'idp'     : 'https://solid.community',
+        'username' : '',
+        'password' : '',
+        'test'     : '/public/test/asw',
+        'ruta'     : ''
+};
+
+
+
+//crear una carpeta
+this.createNewFolder(name, '/public/dechat/');
+
+
+
+
+
