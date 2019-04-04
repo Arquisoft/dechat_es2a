ea9a0a955effa68bef8b14e8a886d8ada8cbbeb9
RaulGarciaArenas <UO257275@uniovi.es>
2019-04-04 14:26:07 +0200

    Tests

---
+++ src/test/asw/steps/LogueoInrupt.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+When('Pagina inicial {string}', function (site) {
+    return browser.get(site);
+});
+
+When('Selecciono Inrupt y Go', function() {
+    var selectDropdownbyNum = function ( element, eleccion ) {
+    if (eleccion){
+      var elijo = element.findElements(by.tagName('name'))   
+        .then(function(options){
+          elijo[eleccion].click();
+        });
+    }
+  };
+    var providerLink = element(by.id('provider'));
+    return providerLink.click();
+ 
+	selectDropdownbyNum(element,1);
+    var link = element(by.id('Go'));
+    return link.click();
+});
