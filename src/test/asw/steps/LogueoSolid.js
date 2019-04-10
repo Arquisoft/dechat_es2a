930013362fe194750f4ea7effad509b3ac9a7e64
RaulGarciaArenas <UO257275@uniovi.es>
2019-04-10 21:26:56 +0200

    Test

---
+++ src/test/asw/steps/LogueoSolid.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+When('Pagina inicial de logueo {string}', function (site) {
+    return browser.get(site);
+});
+
+When('Elijo solid community y hago click en go', function() {
+	var selectDropdownbyNum = function ( element, eleccion ) {
+    if (eleccion){
+      var elijo = element.findElements(by.tagName('name'))   
+        .then(function(elijo){
+            elijo[eleccion].click();
+        });
+    }
+  };
+    var providerLink = element(by.id('provider'));
+    return providerLink.click();
+	selectDropdownbyNum(element,2);
+    var link = element(by.id('Go'));
+    return link.click();
+});
